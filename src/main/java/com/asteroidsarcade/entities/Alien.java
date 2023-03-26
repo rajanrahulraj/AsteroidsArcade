c9d6c1397b881563f7fe4bcdbdb79ff0f263f10a
EchoCharlieII <o1161o5278@gmail.com>
2023-03-26 17:24:37 +0100

    Update Alien.java

--- src/main/java/com/asteroidsarcade/entities/Alien.java
+++ src/main/java/com/asteroidsarcade/entities/Alien.java
@@ -2,6 +2,15 @@ package com.asteroidsarcade.entities;
 
 import com.asteroidsarcade.interfaces.Moveable;
 
-public class Alien extends SpaceShip implements Moveable{
+import javafx.scene.shape.Polygon;
+
+public class Alien extends SpaceShip{
+
+    public Alien(Polygon polygon, int x, int y) {
+        super(polygon, x, y);
+    }
+
+    public Alien() {
+    }
 
 }
