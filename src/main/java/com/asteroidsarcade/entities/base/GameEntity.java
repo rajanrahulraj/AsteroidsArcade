6435afc791c6afdc3d56597e39fcf7c60a14d25b
EchoCharlieII <o1161o5278@gmail.com>
2023-03-26 17:24:22 +0100

    Update GameEntity.java

--- src/main/java/com/asteroidsarcade/entities/base/GameEntity.java
+++ src/main/java/com/asteroidsarcade/entities/base/GameEntity.java
@@ -1,4 +1,5 @@
 package com.asteroidsarcade.entities.base;
+
 import com.asteroidsarcade.interfaces.Moveable;
 import com.asteroidsarcade.main.HelloApplication;
 import com.asteroidsarcade.main.HelloApplication;
@@ -7,47 +8,49 @@ import javafx.scene.shape.Polygon;
 
 public abstract class GameEntity implements Moveable {
 
-        public Polygon entityShape;
+    public Polygon entityShape;
+    public double velocityX;
+    public double velocityY;
 
-        public GameEntity(Polygon polygon, int x, int y) {
-            this.entityShape = polygon;
-            this.entityShape.setTranslateX(x);
-            this.entityShape.setTranslateY(y);
+    public GameEntity(Polygon polygon, int x, int y) {
+        this.entityShape = polygon;
+        this.entityShape.setTranslateX(x);
+        this.entityShape.setTranslateY(y);
+    }
+
+    public Polygon getEntityShape() {
+        return entityShape;
+    }
+
+    public void setVelocity(double x, double y) {
+        this.velocityX = x;
+        this.velocityY = y;
+    }
+
+    public void move() {
+        double changeX = Math.cos(Math.toRadians(this.entityShape.getRotate()));
+        double changeY = Math.sin(Math.toRadians(this.entityShape.getRotate()));
+        entityShape.setTranslateX(entityShape.getTranslateX() + changeX);
+        entityShape.setTranslateY(entityShape.getTranslateY() + changeY);
+
+        // code below added by liaoliao.
+        // code below are used to set all entities stay within the screen.
+        if (this.entityShape.getTranslateX() < 0) {
+            this.entityShape.setTranslateX(this.entityShape.getTranslateX() + HelloApplication.WIDTH);
         }
 
-        public Polygon getEntityShape() {
-            return entityShape;
+        if (this.entityShape.getTranslateX() > HelloApplication.WIDTH) {
+            this.entityShape.setTranslateX(this.entityShape.getTranslateX() % HelloApplication.WIDTH);
         }
 
-        public void setVelocity(){
-            // To-Do
+        if (this.entityShape.getTranslateY() < 0) {
+            this.entityShape.setTranslateY(this.entityShape.getTranslateY() + HelloApplication.HEIGHT);
         }
 
-        public void move() {
-            double changeX = Math.cos(Math.toRadians(this.entityShape.getRotate()));
-            double changeY = Math.sin(Math.toRadians(this.entityShape.getRotate()));
-            entityShape.setTranslateX(entityShape.getTranslateX() + changeX);
-            entityShape.setTranslateY(entityShape.getTranslateY() + changeY);
-         
-            
-         // code below added by liaoliao.
-         // code below are used to set all entities stay within the screen.
-            if (this.entityShape.getTranslateX() < 0) {
-                this.entityShape.setTranslateX(this.entityShape.getTranslateX() + HelloApplication.WIDTH);
-            }
-
-            if (this.entityShape.getTranslateX() > HelloApplication.WIDTH) {
-                this.entityShape.setTranslateX(this.entityShape.getTranslateX() % HelloApplication.WIDTH);
-            }
-
-            if (this.entityShape.getTranslateY() < 0) {
-                this.entityShape.setTranslateY(this.entityShape.getTranslateY() + HelloApplication.HEIGHT);
-            }
-
-            if (this.entityShape.getTranslateY() > HelloApplication.HEIGHT) {
-                this.entityShape.setTranslateY(this.entityShape.getTranslateY() % HelloApplication.HEIGHT);
-            }
-         // code above added by liaoliao.
-            
+        if (this.entityShape.getTranslateY() > HelloApplication.HEIGHT) {
+            this.entityShape.setTranslateY(this.entityShape.getTranslateY() % HelloApplication.HEIGHT);
         }
+        // code above added by liaoliao.
+
+    }
 }
