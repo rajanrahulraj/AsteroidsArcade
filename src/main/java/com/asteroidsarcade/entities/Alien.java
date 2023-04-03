e02d256aad81aa5817e66f2f7281261b1cfd499b
EchoCharlieII <o1161o5278@gmail.com>
2023-04-03 13:45:11 +0100

    Update Alien.java

--- src/main/java/com/asteroidsarcade/entities/Alien.java
+++ src/main/java/com/asteroidsarcade/entities/Alien.java
@@ -9,14 +9,18 @@ public class Alien extends SpaceShip {
 
     public Point2D position;
     public double moveAngle;
-
-    public Alien(int x, int y) {
-        super(new Polygon(0.0, 80.0, 20.0, 0.0, 60.0, 0.0, 80.0, 80.0, 60.0, 160.0, 20.0, 160.0), 0, 0);
-        // this.position = new Point2D(Math.random(), Math.random());
-        this.moveAngle=Math.random()*2*Math.PI;
-    }
+    public double velocity = 10;// pixels per second, not sure if this is right
 
     public Alien() {
+        super(new Polygon(0.0, 80.0, 20.0, 0.0, 60.0, 0.0, 80.0, 80.0, 60.0, 160.0, 20.0, 160.0), 0, 0);
+        // dont know what the last two parameters are
+        this.position = new Point2D(Math.random(), Math.random());
+        
+        this.moveAngle = Math.random() * 2 * Math.PI;
+        this.velocityX = Math.cos(this.moveAngle) * velocity;
+        this.velocityY = Math.sin(this.moveAngle) * velocity;
+
+        move();// Alien starts moving immediately after creation
     }
 
     @Override
