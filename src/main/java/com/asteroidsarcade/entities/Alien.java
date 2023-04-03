9e16069c1eaa875e953f84b65919fb21878d06c6
EchoCharlieII <o1161o5278@gmail.com>
2023-04-03 13:21:54 +0100

    Update Alien.java

--- src/main/java/com/asteroidsarcade/entities/Alien.java
+++ src/main/java/com/asteroidsarcade/entities/Alien.java
@@ -8,10 +8,12 @@ import javafx.scene.shape.Polygon;
 public class Alien extends SpaceShip {
 
     public Point2D position;
+    public double moveAngle;
 
     public Alien(int x, int y) {
-        super(new Polygon(0.0, 80.0, 20.0, 0.0, 60.0, 0.0, 80.0, 80.0, 60.0, 160.0, 20.0, 160.0), x, y);
-        this.position = new Point2D(0, 0);
+        super(new Polygon(0.0, 80.0, 20.0, 0.0, 60.0, 0.0, 80.0, 80.0, 60.0, 160.0, 20.0, 160.0), 0, 0);
+        // this.position = new Point2D(Math.random(), Math.random());
+        this.moveAngle=Math.random()*2*Math.PI;
     }
 
     public Alien() {
@@ -19,8 +21,8 @@ public class Alien extends SpaceShip {
 
     @Override
     public void move() {
-        this.entityShape.setTranslateX(this.entityShape.getTranslateX() + this.position.getX());
-        this.entityShape.setTranslateY(this.entityShape.getTranslateY() + this.position.getY());
+        this.entityShape.setTranslateX(this.entityShape.getTranslateX() + Math.cos(this.moveAngle));
+        this.entityShape.setTranslateY(this.entityShape.getTranslateY() + Math.sin(this.moveAngle));
     }
 
     public Bullet fire() {
