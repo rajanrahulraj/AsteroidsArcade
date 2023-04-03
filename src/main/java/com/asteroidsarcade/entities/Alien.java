6a3b24aebceafb20167fb656fb8f26e4df3d815f
EchoCharlieII <o1161o5278@gmail.com>
2023-04-03 12:52:48 +0100

    Update Alien.java

--- src/main/java/com/asteroidsarcade/entities/Alien.java
+++ src/main/java/com/asteroidsarcade/entities/Alien.java
@@ -1,22 +1,23 @@
 package com.asteroidsarcade.entities;
+
 import com.asteroidsarcade.interfaces.Moveable;
 
 import javafx.geometry.Point2D;
 import javafx.scene.shape.Polygon;
 
-public class Alien extends SpaceShip{
+public class Alien extends SpaceShip {
 
     public Point2D position;
 
     public Alien(int x, int y) {
-        super(new Polygon(-5, -5, 10, 0, -5, 5), x, y);
-        this.position=new Point2D(0, 0);
+        super(new Polygon(0.0, 80.0, 20.0, 0.0, 60.0, 0.0, 80.0, 80.0, 60.0, 160.0, 20.0, 160.0), x, y);
+        this.position = new Point2D(0, 0);
     }
 
     public Alien() {
     }
 
-    public void move(){
+    public void move() {
 
     }
 }
